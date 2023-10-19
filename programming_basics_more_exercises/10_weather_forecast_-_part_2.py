149fa050198b24332b5ed1d92ec5183fbdaab1b6
Yordan Zhelyazkov <147487502+dasho-hub@users.noreply.github.com>
2023-10-19 11:08:39 +0000

    reformat @dasho-hub

--- programming_basics_more_exercises/10_weather_forecast_-_part_2.py
+++ programming_basics_more_exercises/10_weather_forecast_-_part_2.py
@@ -1,14 +1,14 @@
 degrees = float(input())
 
 if 5 <= degrees <= 11.9:
-	print("Cold")
+    print("Cold")
 elif 12 <= degrees <= 14.9:
-	print("Cool")
+    print("Cool")
 elif 15 <= degrees <= 20:
     print("Mild")
 elif 20.1 <= degrees <= 25.9:
-	print("Warm")
+    print("Warm")
 elif 26 <= degrees <= 35:
-	print("Hot")
+    print("Hot")
 else:
-	print("unknown")
+    print("unknown")
