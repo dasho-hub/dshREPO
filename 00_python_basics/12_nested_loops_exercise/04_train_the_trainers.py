8ead56cc78bce8d4c7c87c4b9e125edcf2f48164
dasho-hub <dashobg@gmail.com>
2023-11-11 11:24:49 +0200

    some new files & changes @dasho-hub

--- 00_python_basics/12_nested_loops_exercise/04_train_the_trainers.py
+++ 00_python_basics/12_nested_loops_exercise/04_train_the_trainers.py
@@ -1,3 +1,20 @@
 jury_count = int(input())
-presentation = input()
+pres_counter = 0
 command = input()
+total_total_score = 0
+
+while command != "Finish":
+    presentation = command
+    pres_counter += 1
+    total_score = 0
+    for _ in range(jury_count):
+        total_score += float(input())
+
+    avg_score = total_score / jury_count
+    print(f"{presentation} - {avg_score :.2f}.")
+
+    total_total_score += total_score
+    command = input()
+
+total_avg_score = total_total_score / (pres_counter * jury_count)
+print(f"Student's final assessment is {total_avg_score :.2f}.")
